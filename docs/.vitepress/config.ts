4b66d60e191294a4d15b13fe2580934d88a7858a
guo-zi-xin <1992059868@qq.com>
2023-10-19 17:17:03 +0800

    fix(config): 更新配置文件的路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,7 +17,7 @@ const links: { url: string; lastmod: PageData['lastUpdated'] }[] = []
 
 export default defineConfig({
   outDir: '../dist',
-  base: process.env.APP_BASE_PATH || '/',
+  base: process.env.APP_BASE_PATH || '/web-docs/',
 
   lang: 'zh-CN',
   title: '我的胃来食',
