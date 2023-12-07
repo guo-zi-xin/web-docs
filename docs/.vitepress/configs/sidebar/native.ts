7555568967cd2484edfa396ccc12cca2d70c8465
guo-zi-xin <1992059868@qq.com>
2023-12-07 19:15:25 +0800

    feat(array):添加数组常用方法

--- docs/.vitepress/configs/sidebar/native.ts
+++ docs/.vitepress/configs/sidebar/native.ts
@@ -5,31 +5,39 @@ export const NativeBar = [
     items: [
       { text: '变量的提升', link: '/native/javascript/变量的提升' },
       { text: '回流与重绘', link: '/native/javascript/回流与重绘' },
-      { text: 'call bind apply的区别', link: "/native/javascript/call-apply-bind的区别" },
-      { text: "JS中的元素尺寸与布局信息", link: "/native/javascript/JS中的元素尺寸与布局信息" },
-      { text: "JS模块化", link: "/native/javascript/js模块化" },
-      { text: "moment使用", link: "/native/javascript/moment使用" },
-      { text: "获取当前页面URL信息", link: "/native/javascript/获取当前页面URL信息" },
-      { text: "eslint常见配置", link: "/native/javascript/eslint常见配置" },
-      { text: "JS解析URL参数", link: "/native/javascript/JS解析URL参数" },
-      { text: "JS下载文件", link: "/native/javascript/JS下载文件" },
-      { text: "cookie参数", link: "/native/javascript/cookie参数" },
-      { text: "Array.map与Array.forEach的区别", link: "/native/javascript/map与forEach的区别.md" },
+      { text: 'call bind apply的区别', link: '/native/javascript/call-apply-bind的区别' },
+      { text: 'JS中的元素尺寸与布局信息', link: '/native/javascript/JS中的元素尺寸与布局信息' },
+      { text: 'JS模块化', link: '/native/javascript/js模块化' },
+      { text: 'moment使用', link: '/native/javascript/moment使用' },
+      { text: '获取当前页面URL信息', link: '/native/javascript/获取当前页面URL信息' },
+      { text: 'eslint常见配置', link: '/native/javascript/eslint常见配置' },
+      { text: 'JS解析URL参数', link: '/native/javascript/JS解析URL参数' },
+      { text: 'JS下载文件', link: '/native/javascript/JS下载文件' },
+      { text: 'cookie参数', link: '/native/javascript/cookie参数' },
+      { text: '前端数据类型', link: '/native/javascript/前端数据类型' },
+      { 
+        text: '数组相关',
+        items: [
+          { text: '常用的数组的方法', link: '/native/javascript/Array/常用的数组的方法' },
+          { text: 'Array.map与Array.forEach的区别', link: '/native/javascript/Array/map与forEach的区别' },
+        ],
+        collapsed: false,
+      },
     ],
     collapsed: false,
   },
 
   // CSS
   {
-    text: "CSS",
+    text: 'CSS',
     items: [
-      { text: "CSS权重规则", link: "/native/css/css权重规则" },
-      { text: "css一些加载样式", link: "/native/css/css一些加载样式" },
-      { text: "css跳动方块", link: "/native/css/css跳动方块" },
-      { text: "box-shadow一些示例", link: "/native/css/css-box-shdow" },
-      { text: "3D数字动画", link: "/native/css/3D数字动画" },
-      { text: "炫目动画", link: "/native/css/炫目动画" },
-      { text: "css背景动画", link: "/native/css/css背景动画" },
+      { text: 'CSS权重规则', link: '/native/css/css权重规则' },
+      { text: 'css一些加载样式', link: '/native/css/css一些加载样式' },
+      { text: 'css跳动方块', link: '/native/css/css跳动方块' },
+      { text: 'box-shadow一些示例', link: '/native/css/css-box-shdow' },
+      { text: '3D数字动画', link: '/native/css/3D数字动画' },
+      { text: '炫目动画', link: '/native/css/炫目动画' },
+      { text: 'css背景动画', link: '/native/css/css背景动画' },
 
     ],
     collapsed: false,
@@ -37,30 +45,30 @@ export const NativeBar = [
 
   // Node
   {
-    text: "Node",
-    items: [{ text: "Node简介", link: "/native/node/node简介" }],
+    text: 'Node',
+    items: [{ text: 'Node简介', link: '/native/node/node简介' }],
     collapsed: false,
   },
 
   // TypeSccript
   {
-    text: "Typescript",
+    text: 'Typescript',
     items: [
-      { text: "TypeScript推荐文档", link: "/native/typescript/推荐文档" },
-      { text: "TypeScript泛型", link: "/native/typescript/泛型" },
-      { text: "TypeScript枚举", link: "/native/typescript/枚举" },
-      { text: "TypeScript类型体操", link: "/native/typescript/类型体操" },
+      { text: 'TypeScript推荐文档', link: '/native/typescript/推荐文档' },
+      { text: 'TypeScript泛型', link: '/native/typescript/泛型' },
+      { text: 'TypeScript枚举', link: '/native/typescript/枚举' },
+      { text: 'TypeScript类型体操', link: '/native/typescript/类型体操' },
     ],
     collapsed: false,
   },
 
   // 小程序
   {
-    text: "微信小程序相关",
+    text: '微信小程序相关',
     items: [
       {
-        text: "微信小程序人脸识别问题",
-        link: "/native/weapp/微信小程序人脸识别问题",
+        text: '微信小程序人脸识别问题',
+        link: '/native/weapp/微信小程序人脸识别问题',
       },
     ],
     collapsed: false,
