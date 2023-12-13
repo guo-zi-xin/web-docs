949485c2361a09feef6faabfbf68c736bc9d0848
guo-zi-xin <1992059868@qq.com>
2023-12-13 18:33:54 +0800

    feat(weekly):调整先前导言目录结构

--- docs/.vitepress/configs/sidebar/guide.ts
+++ docs/.vitepress/configs/sidebar/guide.ts
@@ -4,12 +4,17 @@ export const GuideBar = [
     link: '/guide/home',
   },
   {
-    text: 'Accumulation',
+    text: '周报',
     items: [
       {
-        text: 'weekly',
-        link: '/guide/weekly',
-      }
-    ]
-  },
+        text: '231204-231208',
+        link: '/guide/weekly/231204-231208',
+      },
+      {
+        text: '231211-231215',
+        link: '/guide/weekly/231211-231215',
+      },
+    ],
+    collapsed:false
+  }
 ]
