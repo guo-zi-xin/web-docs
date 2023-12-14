fa6a2b997b68c35444b52cfd67d70e49c2d51380
guo-zi-xin <1992059868@qq.com>
2023-12-14 19:35:23 +0800

    feat(sidebar): 添加面试相关子模块

---
+++ docs/.vitepress/configs/sidebar/interview.ts
@@ -0,0 +1,17 @@
+export const InterViewBar = [
+  {
+    text: '面试相关',
+    items: [
+      {
+        text: '前端基础',
+        link: '/interview/base/index',
+        items: [
+          {
+            text: 'HTML CSS 浏览器相关',
+            link: '/interview/base/native',
+          }
+        ]
+      }
+    ]
+  },
+]
