f0462adce453b248585219d17035da5c6f40ca37
guozixin@cmcm.com <guozixin@cmcm.com>
2024-02-23 14:32:22 +0800

    feat(react):添加react组件相关

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -7,19 +7,14 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    'Bi:fileEarmarkWordFill': typeof import('~icons/bi/file-earmark-word-fill')['default']
     'Bi:github': typeof import('~icons/bi/github')['default']
     CustomLink: typeof import('./theme/components/CustomLink.vue')['default']
-    'EosIcons:modifiedDate': typeof import('~icons/eos-icons/modified-date')['default']
     'Fe:youtube': typeof import('~icons/fe/youtube')['default']
     'IconPark:addWeb': typeof import('~icons/icon-park/add-web')['default']
     'Logos:momentjs': typeof import('~icons/logos/momentjs')['default']
     'Logos:reactQueryIcon': typeof import('~icons/logos/react-query-icon')['default']
-    'Octicon:feedPerson16': typeof import('~icons/octicon/feed-person16')['default']
-    'Ooui:clock': typeof import('~icons/ooui/clock')['default']
     PageInfo: typeof import('./theme/components/PageInfo.vue')['default']
     'Ph:planetFill': typeof import('~icons/ph/planet-fill')['default']
-    'RadixIcons:update': typeof import('~icons/radix-icons/update')['default']
     'SkillIcons:reactDark': typeof import('~icons/skill-icons/react-dark')['default']
     'SkillIcons:stackoverflowDark': typeof import('~icons/skill-icons/stackoverflow-dark')['default']
     'SkillIcons:typescript': typeof import('~icons/skill-icons/typescript')['default']
