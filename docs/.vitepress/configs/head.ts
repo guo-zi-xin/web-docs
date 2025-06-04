d41e1c3684a41f1aa26bc88924b1b47fcac98889
guo-zi-xin <1992059868@qq.com>
2025-06-04 15:15:26 +0800

    feat(logo):修改标签meta logo

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,10 +1,10 @@
 import type { HeadConfig } from 'vitepress'
 
-const isDevelopment = process.env.NODE_ENV === 'development'
+const base = process.env.APP_BASE_PATH || '/web-docs/'
 
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/svg/blog-logo.svg', type:'image/svg+xml'}],
-  ['link', { rel: 'icon', href: '/image/blog-logo.png', type: 'image/png'}],
+  ['link', { rel: 'icon', href: `${base}image/blog-logo.svg`, type:'image/svg+xml'}],
+  ['link', { rel: 'icon', href: `${base}image/blog-logo.png`, type: 'image/png'}],
   ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'mobile-web-app-capable', content: 'yes' }],
