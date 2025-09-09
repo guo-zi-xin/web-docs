410386440a2fba19f968f9a4f41a05f6d226cd09
guo-zi-xin <1992059868@qq.com>
2025-09-09 20:17:23 +0800

    feat(native):generator函数更新

--- docs/.vitepress/configs/sidebar/interview.ts
+++ docs/.vitepress/configs/sidebar/interview.ts
@@ -46,34 +46,34 @@ export const InterViewBar = [
           }
         ]
       },
-      {
-        text: '前端基础',
-        link: '/interview/base/index',
-        items: [
-          {
-            text: 'HTML CSS 浏览器相关',
-            link: '/interview/base/native',
-          },
-          {
-            text: 'JavaScript相关',
-            link: '/interview/base/javascript',
-          }
-        ]
-      },
-      {
-        text: '框架相关',
-        link: '/interview/frame/index',
-        items: [
-          {
-            text: 'Vue',
-            link: '/interview/frame/vue',
-          },
-          {
-            text: 'React',
-            link: '/interview/frame/react',
-          }
-        ]
-      },
+      // {
+      //   text: '前端基础',
+      //   link: '/interview/base/index',
+      //   items: [
+      //     {
+      //       text: 'HTML CSS 浏览器相关',
+      //       link: '/interview/base/native',
+      //     },
+      //     {
+      //       text: 'JavaScript相关',
+      //       link: '/interview/base/javascript',
+      //     }
+      //   ]
+      // },
+      // {
+      //   text: '框架相关',
+      //   link: '/interview/frame/index',
+      //   items: [
+      //     {
+      //       text: 'Vue',
+      //       link: '/interview/frame/vue',
+      //     },
+      //     {
+      //       text: 'React',
+      //       link: '/interview/frame/react',
+      //     }
+      //   ]
+      // },
       // {
       //   text: '小程序相关',
       //   link:'/interview/applet'
